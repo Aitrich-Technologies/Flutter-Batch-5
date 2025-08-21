0238d8b137f0562e912068b3356c97156cc2d8ae
thasnikasim <thasniaitrich@gmail.com>
2025-08-21 09:57:17 +0530

    added module 3 workshops and its handouts

---
+++ flutter/Module 3 Dart Foundation/Chapter 2/workshop/workshop6/bin/workshop6.dart
@@ -0,0 +1,24 @@
+void main(List<String> args) {
+  var obj = cs();
+  print(obj.cs_String);
+  print(obj.str);
+  print(obj.str);
+  }
+
+
+  class cs extends cs1 {
+    String cs_String = 'This is from derived class';
+  }
+
+  class cs1 extends cs2 {
+    String str = 'This is from class one';
+  }
+
+ class cs2{
+<<<<<<< HEAD
+  String str1 = 'This is from class two';
+=======
+  
+>>>>>>> f50df7398969509bd78cf191b9b8db515e1bd804
+ }
+  
